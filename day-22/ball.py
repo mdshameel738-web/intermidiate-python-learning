fe47ad9e661f4f05d664d6686f3a472418765468
mdshameel738-web <mdshameel738@gmail.com>
2026-07-19 03:51:35 -0400

    Add files via upload

---
+++ day-22/ball.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move =10
+        self.y_move =10
+        self.s=0.1
+        
+    def move(self):
+        new_x=self.xcor()+self.x_move
+        new_y=self.ycor()+self.y_move
+        self.goto(new_x,new_y)
+      
+    def bouncey(self):
+        self.y_move *= -1
+        self.s*=0.9
+    def bouncex(self):
+        self.x_move *= -1
+        self.s*=0.9
+    
+    def reset_position(self):
+       self.goto(0,0)
+       self.s=0.1
+       self.bouncex()
+
+       
